fee2f5fe42388fd9bdbc2f15acb3bfefb1e65f66
Sean Soper <sean.soper@gmail.com>
2019-12-24 09:58:14 -0500

    varargs, spread operators, slicing arrays

--- src/WatchFile.kt
+++ src/WatchFile.kt
@@ -2,12 +2,32 @@ import java.nio.file.FileSystems
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY
+import java.nio.file.WatchService
+
+fun registerPaths(paths: Array<String>): WatchService {
+    val watchService = FileSystems.getDefault().newWatchService()
+
+    if (paths.count() > 1) {
+        val otherPaths = paths.sliceArray(IntRange(1, paths.count()-1))
+        val watchPath = Paths.get(paths.first(), *otherPaths)
+        watchPath.register(watchService, ENTRY_MODIFY)
+    } else {
+        val watchPath = Paths.get(paths.first())
+        watchPath.register(watchService, ENTRY_MODIFY)
+    }
+
+    return watchService
+}
 
 fun main(args: Array<String>) {
-    val watchService = FileSystems.getDefault().newWatchService()
-    val path = Paths.get("/Users/ssoper/workspace/StaticSite/css")
-    path.register(watchService, ENTRY_MODIFY)
 
+    val mine = arrayOf("sean", "is", "awesome")
+    val otherPaths = mine.sliceArray(IntRange(1, mine.count()-1))
+    mine.forEach { println(it) }
+    println("oh hai")
+    otherPaths.forEach { println(it) }
+
+/*
     println(path)
 
     while (true) {
@@ -20,5 +40,5 @@ fun main(args: Array<String>) {
         }
         key.reset()
 
-    }
+    }*/
 }
