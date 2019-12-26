de5775ec6280984fb400419b635f7df60f2845e0
Sean Soper <sean.soper@gmail.com>
2019-12-25 21:27:18 -0500

    basic functionality implemented, watches mult files, uses channels/coroutines tho perhaps unnecessary for this simple case

--- src/main/kotlin/WatchFile.kt
+++ src/main/kotlin/WatchFile.kt
@@ -1,3 +1,4 @@
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import java.nio.file.FileSystems
 import java.nio.file.Path
@@ -5,36 +6,47 @@ import java.nio.file.Paths
 import java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY
 import java.nio.file.WatchService
 
-fun registerPaths(paths: Array<String>): List<Channel<String>> {
-    return paths.map {
-        val channel = Channel<String>()
-        val watchService = FileSystems.getDefault().newWatchService()
+fun registerPaths(paths: Array<String>): WatchService {
+    val watchService = FileSystems.getDefault().newWatchService()
+
+    paths.forEach {
         val watchPath = Paths.get(it)
         watchPath.register(watchService, ENTRY_MODIFY)
+    }
 
+    return watchService
+}
+
+suspend fun createChannel(service: WatchService): Channel<String> {
+    val channel = Channel<String>()
+    val scope: CoroutineScope = GlobalScope
+
+    scope.launch(Dispatchers.IO) {
         while (true) {
-            println("watching")
-            val key = watchService.take()
+            val key = service.take()
             val dirPath = key.watchable() as? Path ?: break
             key.pollEvents().forEach {
                 val eventPath = dirPath.resolve(it.context() as Path)
-                println("Path $eventPath, context $it.context()")
+                channel.send(eventPath.toString())
             }
             key.reset()
-
         }
-
-        return listOf(channel)
     }
 
+    return channel
 }
 
-fun main(args: Array<String>) {
+fun main() = runBlocking{
 
     val paths = arrayOf(
-        "/Users/ssoper/workspace/StaticSite/css"
+        "/Users/ssoper/workspace/StaticSite/css",
+        "/Users/ssoper/workspace/StaticSite/js"
     )
 
-    val watchService = registerPaths(paths)
+    val service = registerPaths(paths)
+    val channel = createChannel(service)
 
+    while (true) {
+        println("Path ${channel.receive()}")
+    }
 }
