b755754f3582dbe31614ab88a416ced62b17158d
Sean Soper <sean.soper@gmail.com>
2020-01-14 20:42:41 -0500

    added publishing capability, published a JAR, verified it worked

--- README.md
+++ README.md
@@ -2,24 +2,23 @@
 
 # Zebec
 
-Simple static site generator. Written in Kotlin.
+Static site compiler. Written in Kotlin.
 
 ## Features
 
-* HTML files compiled from Kotlin DSL (KTML).
-* Includes minifier for CSS and JavaScript files. 
-* File watcher service that automatically compiles changed files.
-* Local development facilitated by built-in web server that refreshes pages for changed files.
+* HTML files compiled from Kotlin-based DSL (KTML) 🔧
+* Includes a [minifier](https://yui.github.io/yuicompressor/) for CSS and JavaScript files 🗜
+* Watches for changed files that are automatically compiled or minified 👁 
+* Ships with a tiny web server to make local development a cinch 💅
+* [Downloadable JAR](https://github.com/ssoper/Zebec/packages) that runs as a service 📦
 
 ## Roadmap
 
 * Blogging
 * Keywords/Tagging
 * Templating
-* Additional configurations i.e. turn off auto refreshing
 * Generate site files
     * [robots.txt](https://support.google.com/webmasters/answer/6062596?hl=en&ref_topic=6061961)
     * [sitemap](https://support.google.com/webmasters/answer/183668?hl=en&ref_topic=4581190)
 * Generate shields.io badges (examples: w3c validation, link backs, etc.)
-* Publish a JAR
 * Saved state of compiled files via local database (SQL file)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
+import java.io.FileInputStream
+import java.util.*
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.61"
+    id("maven-publish")
 }
-
-group = "com.seansoper.Zebec"
-version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
-var jsr223WorkingVersion = "1.3.21"
+val jsr223WorkingVersion = "1.3.21"
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -18,6 +19,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:$jsr223WorkingVersion")
     implementation("org.jetbrains.kotlin:kotlin-script-util:$jsr223WorkingVersion")
     compile(files("src/main/libs/yuicompressor-2.4.8.jar"))
+
 }
 
 tasks.compileKotlin {
@@ -26,4 +28,32 @@ tasks.compileKotlin {
 
 tasks.compileTestKotlin {
     this.kotlinOptions.jvmTarget = "1.8"
+}
+
+val githubProperties = Properties()
+githubProperties.load(FileInputStream(rootProject.file("github.properties")))
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            run {
+                groupId = "com.seansoper"
+                artifactId = "Zebec"
+                version = "1.0"
+                artifact("$rootDir/out/artifacts/${artifactId}_main_jar/$artifactId.jar")
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/ssoper/Zebec") // Github Package
+            credentials {
+                //Fetch these details from the properties file or from Environment variables
+                username = githubProperties.get("gpr.usr") as String? ?: System.getenv("GPR_USER")
+                password = githubProperties.get("gpr.key") as String? ?: System.getenv("GPR_API_KEY")
+            }
+        }
+    }
 }
