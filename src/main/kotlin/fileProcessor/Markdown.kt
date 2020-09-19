e29ea50367197ad62d23036ce185e20a2b8afe66
Sean Soper <sean.soper@gmail.com>
2020-09-19 13:35:58 -0400

    strips link urls from first paragraph showing only the link text

--- src/main/kotlin/fileProcessor/Markdown.kt
+++ src/main/kotlin/fileProcessor/Markdown.kt
@@ -1,5 +1,8 @@
 package com.seansoper.zebec.fileProcessor
 
+import org.intellij.markdown.IElementType
+import org.intellij.markdown.MarkdownElementTypes
+import org.intellij.markdown.ast.findChildOfType
 import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
 import org.intellij.markdown.html.HtmlGenerator
 import org.intellij.markdown.parser.MarkdownParser
@@ -13,12 +16,27 @@ class Markdown(): Processable {
         return HtmlGenerator(content, parser, Flavor).generateHtml()
     }
 
+    // TODO: Update to account for bold, italics, etc in first paragraph
+    // TODO: Add tests
+    //       https://github.com/valich/intellij-markdown/blob/master/src/org/intellij/markdown/MarkdownElementTypes.kt
     fun getFirstParagraph(content: String): String? {
         val parser = MarkdownParser(Flavor).buildMarkdownTreeFromString(content)
         return parser.children.firstOrNull {
             it.type.toString() == "Markdown:PARAGRAPH"
         }?.let {
-            content.substring(it.startOffset, it.endOffset)
+            var text = ""
+
+            it.children.forEach {
+                if (it.type == MarkdownElementTypes.INLINE_LINK) {
+                    it.findChildOfType(MarkdownElementTypes.LINK_TEXT)?.let {
+                        text += content.substring(it.startOffset+1, it.endOffset-1)
+                    }
+                } else {
+                    text += content.substring(it.startOffset, it.endOffset)
+                }
+            }
+
+            text.replace("\\*", "*")
         }
     }
 
